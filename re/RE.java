c6f2d020f684cd6d4cb42ad7d62c36ea5e79ec68
rich boundji <boundjirichrich@gmail.com>
2022-12-04 21:32:04 -0700

    just starting

---
+++ re/RE.java
@@ -0,0 +1,18 @@
+package re;
+
+import fa.dfa.DFA;
+import fa.nfa.NFA;
+
+public class RE implements REInterface{
+
+    public RE(String str){
+
+
+    }
+
+    @Override
+    public NFA getNFA() {
+
+        return null;
+    }
+}
